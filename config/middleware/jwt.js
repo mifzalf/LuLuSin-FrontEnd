9b8bfab61dbbf96a6773dcd53382aef5af4f7486
DAFAAHMADFAHRISI <dafamangku@gmail.com>
2025-04-12 05:53:24 +0700

    update

--- config/middleware/jwt.js
+++ config/middleware/jwt.js
@@ -1,11 +1,20 @@
 function authorize(AllowedRoles = []) {
     return (req, res, next) => {
         if (!req.user) {
-            return res.status(403).json ({ message: 'User not authentacated'})
+            return res.status(401).json({ message: 'User not authenticated' });
         }
-        if (!AllowedRoles.includes(req.user.userType)) { 
-            return res.status(403).json ({ message: 'Acess denied: You do not have premission'}) 
+
+        console.log('authorize - Checking user:', req.user); 
+        console.log('authorize - Allowed Roles:', AllowedRoles); 
+        console.log('authorize - User Type:', req.user.type); 
+        console.log('authorize - Is role allowed?', AllowedRoles.includes(req.user.type)); 
+
+        if (!req.user.type || !AllowedRoles.includes(req.user.type)) {
+             console.log('authorize - Access Denied!'); 
+             return res.status(403).json({ message: 'Access denied: You do not have permission for this resource' });
         }
-        next()
+
+        console.log('authorize - Access Granted!');
+        next();
     }
 } 
